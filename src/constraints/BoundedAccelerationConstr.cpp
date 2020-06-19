4c43320f32cab0c9b1f933f5131d76a711a95a28
nana <bolotnikova.anastasia@gmail.com>
2020-06-19 10:10:00 +0200

    Custom constraints

---
+++ src/constraints/BoundedAccelerationConstr.cpp
@@ -0,0 +1,27 @@
+#include "BoundedAccelerationConstr.h"
+
+
+
+BoundedAccelerationConstr::BoundedAccelerationConstr(unsigned int rIndex,
+                                                     double maxAccTransXY,
+                                                     double maxAccRotZ)
+: mc_solver::GenInequalityConstraintRobot(rIndex),
+  maxAccTransXY_(maxAccTransXY), maxAccRotZ_(maxAccRotZ)
+  {
+    A_.resize(3, 6);
+    A_.setZero();
+    A_(0, 2) = 1.0;
+    A_(1, 3) = 1.0;
+    A_(2, 4) = 1.0;
+
+    L_.resize(3);
+    L_.setZero();
+    L_(0) = -maxAccRotZ;
+    L_(1) = -maxAccTransXY;
+    L_(2) = -maxAccTransXY;
+
+    U_ = -L_.eval();
+  }
+
+
+  void BoundedAccelerationConstr::compute() { }
---
+++ src/constraints/BoundedAccelerationConstr.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <mc_solver/GenInequalityConstraint.h>
+
+struct BoundedAccelerationConstr : public mc_solver::GenInequalityConstraintRobot{
+
+  BoundedAccelerationConstr(unsigned int rIndex, double maxAccTransXY, double maxAccRotZ);
+
+  inline int maxGenInEq() const override { return 3; }
+
+  inline std::string nameGenInEq() const override { return "BoundedAccelerationConstr"; }
+
+  inline const Eigen::VectorXd & LowerGenInEq() const override { return L_; }
+
+  inline const Eigen::VectorXd & UpperGenInEq() const override { return U_; }
+
+  inline const Eigen::MatrixXd & A() const override { return A_; }
+
+  void compute() override;
+
+  private:
+    // Max translation_XY acceleration
+    double maxAccTransXY_;
+    // Max rotation_Z acceleration
+    double maxAccRotZ_;
+
+    // L_ <= A*x <= U_
+    Eigen::MatrixXd A_;
+    Eigen::VectorXd L_;
+    Eigen::VectorXd U_;
+  };
